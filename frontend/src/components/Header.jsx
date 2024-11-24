3d4a64b0b2c6028a5b07258d783f3ab8b2d8e220
Filipe Silveira Maciel <filipe10sm@gmail.com>
2024-11-23 23:48:36 -0300

    Estilização header relizada

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,38 @@
 export default function Header() {
 
+  const selectedBtn = " bg-black/35 border-t-[0.2rem] border-l-[0.2rem] px-3 border-b-[0.2rem] border-r-[0.2rem] border-t-black/80 border-l-black/80 border-b-verde_botao border-r-verde_botao"
+
   function login() {
     console.log('No futuro vou abrir um modal, com uma pequena tela de login, para então, quando o funcionário estiver logado, liberar uma ou outra funcionalidade que estava antes bloqueada.');
   }
+  function menuHamburger(){
+    console.log("Menuuuuu");
+    
+  }
 
   return (
-    <header className="flex justify-around items-center">
-      <div>
-        <img src="" alt="Logo Coder++" />
-      </div>
+    <header className="flex justify-evenly gap-32 lg:gap-[40rem] items-center bg-background w-full h-20 text-texto_header">
 
-      <div className="flex gap-24">
-        <nav className="flex gap-4">
+      <img className="size-16" src="logo.png" alt="Logo Coder++" />
+
+
+      
+      <div className="flex">
+        <nav className=" gap-4 hidden sm:flex">
           <ul className="flex gap-4">
-            <li>Home</li>
+            <li className={selectedBtn}>Home</li> 
             <li>Freelances</li>
             <li>Projetos</li>
             <li>Cursos</li>
           </ul>
         </nav>
-        <button onClick={login}>Entrar</button>
+      
+      <button onClick={login} className="flex items-center size-10 justify-center  sm:hidden"><img className=" h-12" src="Login.png" alt="" /></button>
+      
+      <button onClick={menuHamburger} className="flex items-center justify-center size-10  sm:hidden"><img className=" size-10" src="Database.png" alt="" /></button>
+     
+      <button className="hidden sm:flex sm:ml-40" onClick={login}>Entrar</button>
+
       </div>
     </header>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-fundo': '#242424',
+        'verde_botao': '#298C00',
+        'background': '#1E1E1E',
+        'texto_header': '#B3B3B3',
+      },
+    },
+    dropShadow: {
+      'botao': 'px 4px 4px rgba(0, 0, 0, 1)',
+      
+    }
   },
   plugins: [],
 }
