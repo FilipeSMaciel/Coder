e18c865934d50e02c4a4453fd872c6492e1960e4
pedrohassen <pedro_hasse@yahoo.com.br>
2024-11-24 17:26:55 -0300

    ✨ (frontend): Add react-hook-form package to manage form state and validation in the project 📝 (frontend): Update Header component to include a LoginModal using react-responsive-modal and react-hook-form for form handling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,58 @@
+import Modal from "react-responsive-modal";
+import LoginModal from "./LoginModal";
+import { useState } from "react";
+
 export default function Header() {
 
   const selectedBtn = " bg-black/35 border-t-[0.2rem] border-l-[0.2rem] px-3 border-b-[0.2rem] border-r-[0.2rem] border-t-black/80 border-l-black/80 border-b-verde_botao border-r-verde_botao"
 
-  function login() {
-    console.log('No futuro vou abrir um modal, com uma pequena tela de login, para então, quando o funcionário estiver logado, liberar uma ou outra funcionalidade que estava antes bloqueada.');
-  }
+  const [open, setOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setOpen(true);
+  };
+
   function menuHamburger() {
     console.log("Menuuuuu");
 
   }
 
   return (
-    <header className="flex justify-evenly drop-shadow-3xl border-b-2 border-verde_principal gap-32 lg:gap-[40rem] items-center bg-background w-full h-20 text-texto_header">
+    <>
+      <header className="flex justify-evenly drop-shadow-3xl border-b-2 border-verde_principal gap-32 lg:gap-[40rem] items-center bg-background w-full h-20 text-texto_header">
 
-      <img className="size-16" src="logo.png" alt="Logo Coder++" />
+        <img className="size-16" src="logo.png" alt="Logo Coder++" />
 
+        <div className="flex">
+          <nav className=" gap-4 hidden sm:flex">
+            <ul className="flex gap-4">
+              <button onClick={""}>
+                <li className={selectedBtn}>Home</li>
+              </button>
+              <button>
+                <li>Freelances</li>
+              </button>
+              <button>
+                <li>Projetos</li>
+              </button>
+              <button>
+                <li>Cursos</li>
+              </button>
+            </ul>
+          </nav>
 
+          <button onClick={openLoginModal} className="flex items-center size-10 justify-center  sm:hidden"><img className=" h-12" src="Login.png" alt="" /></button>
 
-      <div className="flex">
-        <nav className=" gap-4 hidden sm:flex">
-          <ul className="flex gap-4">
-            <button onClick={""}>
-              <li className={selectedBtn}>Home</li>
-            </button>
-            <button>
-              <li>Freelances</li>
-            </button>
-            <button>
-              <li>Projetos</li>
-            </button>
-            <button>
-              <li>Cursos</li>
-            </button>
-          </ul>
-        </nav>
+          <button onClick={menuHamburger} className="flex items-center justify-center size-10  sm:hidden "><img className=" size-10" src="Database.png" alt="" /></button>
 
-        <button onClick={login} className="flex items-center size-10 justify-center  sm:hidden"><img className=" h-12" src="Login.png" alt="" /></button>
+          <button className="hidden lg:flex lg:ml-40 lg:bg-bg_botao-login lg:px-10 lg:items-center lg:justify-center lg:rounded-md hover:scale-105 hover:border-neutral-700 hover:border-[0.1rem] transition-transform duration-100 ease-in-out" onClick={openLoginModal}>Entrar</button>
 
-        <button onClick={menuHamburger} className="flex items-center justify-center size-10  sm:hidden "><img className=" size-10" src="Database.png" alt="" /></button>
+        </div>
+      </header>
 
-        <button className="hidden lg:flex lg:ml-40 lg:bg-bg_botao-login lg:px-10 lg:items-center lg:justify-center lg:rounded-md hover:scale-105 hover:border-neutral-700 hover:border-[0.1rem] transition-transform duration-100 ease-in-out" onClick={login}>Entrar</button>
-
-      </div>
-    </header>
+      <Modal open={open} onClose={() => setOpen(false)} center>
+        <LoginModal />
+      </Modal>
+    </>
   )
 }
--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,6 +1,53 @@
+import { useForm } from "react-hook-form";
+
 export default function LoginModal() {
+  const { register, handleSubmit } = useForm();
 
   return (
-    <h1>Serei um Login algum dia</h1>
+    <>
+      <section>
+        <h1>Login</h1>
+        <h2>Ou se preferir, cadastre-se.</h2>
+
+        <img src="" alt="Logo Coder++" />
+
+        <form>
+          <input type="text" placeholder="Nome de usuário" required />
+
+          <input type="password" placeholder="Senha" required />
+
+          <p className='flex justify-evenly gap-2 '>
+            <input type="submit" value="ENTER" />
+            <input type="reset" value="Limpar" />
+          </p>
+
+          <div>
+            <img src="" alt="Logo do Google" />
+            <img src="" alt="Logo do Github" />
+            <img src="" alt="Logo do Linkedin" />
+          </div>
+        </form>
+
+        <img src="" alt="Imagem abstrata da tela de Login" />
+
+        <h1>Cadastro</h1>
+        <h2>Ou se preferir, faça login.</h2>
+
+        <form>
+          <input type="text" placeholder="Nome de usuário" required />
+
+          <input type="password" placeholder="Senha" required />
+
+          <input type="email" placeholder="Email" />
+
+          <div>
+            <img src="" alt="Logo do Google" />
+            <img src="" alt="Logo do Github" />
+            <img src="" alt="Logo do Linkedin" />
+            <input type="submit" value="ENTER" />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
