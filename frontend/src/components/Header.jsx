d85bb25ccd13e3df03d58a9ac28a76b2c487bbbb
unknown <ericbreinhardt12@gmail.com>
2024-11-26 21:23:33 -0300

    funcionalidade de login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,10 +8,17 @@ export default function Header({ setOpen }) {
     const [username, setUsername] = useState(null);
     const location = useLocation();
     const isActive = (path) => location.pathname === path ? 'bg-black/35 border-t-[0.2rem] border-l-[0.2rem] px-3 border-b-[0.2rem] border-r-[0.2rem] border-t-black/80 border-l-black/80 border-b-verde_botao border-r-verde_botao' : '';
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const homeLink = isLoggedIn ? '/home' : '/';
 
     useEffect(() => {
         const storedUsername = localStorage.getItem("username");
-        if (storedUsername) setUsername(storedUsername);
+        if (storedUsername) {
+            setUsername(storedUsername);
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
     }, []);
 
     const openLoginModal = () => {
@@ -33,13 +40,15 @@ export default function Header({ setOpen }) {
 
     return (
         <header className="flex justify-evenly drop-shadow-3xl border-b-2 border-verde_principal gap-32 lg:gap-[40rem] items-center bg-background w-full h-20 text-texto_header">
+            
             <img className="size-16" src="logo.png" alt="Logo Coder++" />
+
             <div className="flex">
                 <nav className="gap-4 hidden sm:flex">
                     <ul className="flex gap-4">
                         <button>
-                            <Link to="/home">
-                                <li className={isActive("/home")}>
+                            <Link to={homeLink}>
+                                <li className={isActive(location.pathname === "" || location.pathname === "/home" ? "/home" : "/")}>
                                     Home
                                 </li>
                             </Link>
--- frontend/src/pages/Freelances.jsx
+++ frontend/src/pages/Freelances.jsx
@@ -1,8 +1,4 @@
-import Modal from "react-responsive-modal";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
-import LoginModal from "../components/LoginModal";
-import { useState } from "react";
 
 export default function Freelances(){
 
