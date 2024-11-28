7e5accbec8313fbdd73dc9bfaa54279bfc994c10
unknown <pedro_hasse@yahoo.com.br>
2024-11-28 12:20:11 -0300

    Add in more structure to all pages

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import Menu from "../components/Menu";
+import Modal from "react-responsive-modal";
+import LoginModal from "../components/LoginModal";
+import Footer from "../components/Footer";
+
 export default function Profile() {
+  const [open, setOpen] = useState(false);
+
+  const handleLogoff = (username) => {
+    localStorage.setItem("username", username);
+    setOpen(false);
+  };
+
   return (
-    <h1>Perfil</h1>
+    <>
+    <Menu setOpen={setOpen} />
+    <h1>Prerfil</h1>
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <LoginModal onLogin={handleLogoff} />
+    </Modal>
+    <Footer />
+  </>
   );
 }
