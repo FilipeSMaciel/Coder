c2fffe7f918fe4722fad03f1f9f0a77f0f0a6486
Pedro Hasse Niemczewski <pedro_hasse@yahoo.com.br>
2024-11-29 23:08:28 -0300

    Fix login modal image position

--- frontend/src/components/MenuProjetos.jsx
+++ frontend/src/components/MenuProjetos.jsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom";
 
-export default function MenuProjetos () {
+export default function MenuProjetos() {
     const projectsLink = '/projects';
     const profileLink = '/profile';
     const isActive = (path) => {
         console.log(`Checking if ${location.pathname} matches ${path}`);
         return location.pathname === path ? 'bg-black/35 border-t-[0.2rem] border-l-[0.2rem] px-3 border-b-[0.2rem] border-r-[0.2rem] border-t-black/80 border-l-black/80 border-b-verde_botao border-r-verde_botao' : '';
-      };
+    };
 
-    return (  
-        <header>      
+    return (
+        <header>
             <nav className="flex ">
                 <Link to={profileLink} className={`px-4 ${isActive('/profile')}`}
                 >
-                Perfil
+                    Perfil
                 </Link>
-                
+
                 <Link to={projectsLink} className={`px-4${isActive('/projects')}`}
                 >
-                Projetos
+                    Projetos
                 </Link>
             </nav>
         </header>
