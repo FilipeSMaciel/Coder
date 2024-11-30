c4964efafcf61ec620dcb2023431a24ab6db35c4
Pedro Hasse Niemczewski <pedro_hasse@yahoo.com.br>
2024-11-30 02:02:20 -0300

    Freelances page structure

--- frontend/src/pages/Freelances.jsx
+++ frontend/src/pages/Freelances.jsx
@@ -18,16 +18,74 @@ export default function Freelances() {
             <Menu setOpen={setOpen} />
 
             <main>
-                <h1>Procure um freelance {'>>'}</h1>
-                <h2>Pesquise o projeto perfeito para vocé</h2>
+                <section>
+                    <h1>Procure um freelance {'>>'}</h1>
+                    <h2>Pesquise o projeto perfeito para vocé</h2>
+
+                    <div>
+                        <p>Barra de pesquisa placeholder</p>
+                        <button>Pesquisar</button>
+                    </div>
+
+                    <button>Filtros</button>
+                    <button>Order By</button>
+                </section>
+
+                <section>
+                    <h2>Mais vistos recentemente {'>>'}</h2>
+                    <hr />
+                    <h3>Com base nos projetos de mais destaque dessa semana</h3>
+
+                    <div>
+                        <ul>
+                            <li>
+                                <img src="" alt="Logo Projeto" />
+                                <div>
+                                    <h4>Procura-se programadores para Janeiro/25</h4>
+                                    <p>Você deseja participar da CokeJam de Janeiro/25?</p>
+                                    <p>Requisitos: C#, C++, Python, PHP, ORACLE, ANGULAR, JAVA, SQL, Não ser corno</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img src="" alt="Contador de devs icon" />
+                                        <p>15</p>
+                                    </div>
+                                    <a href="#">Saiba Mais ...</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+
+                <section>
+                    <h2>Para você {'>>'}</h2>
+                    <hr />
+                    <h3>Com base nas suas competências e preferências</h3>
+
+                    <div>
+                        <ul>
+                            <li>
+                                <img src="" alt="Logo Projeto" />
+                                <div>
+                                    <h4>Procura-se programadores para Janeiro/25</h4>
+                                    <p>Você deseja participar da CokeJam de Janeiro/25?</p>
+                                    <p>Requisitos: C#, C++, Python, PHP, ORACLE, ANGULAR, JAVA, SQL, Não ser corno</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <img src="" alt="Contador de devs icon" />
+                                        <p>15</p>
+                                    </div>
+                                    <a href="#">Saiba Mais ...</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
 
                 <div>
-                    <p>Barra de pesquisa placeholder</p>
-                    <button>Pesquisar</button>
+                    <p>Próxima página placeholder</p>
                 </div>
-
-                <button>Filtros</button>
-                <button>Order By</button>
             </main>
 
             <Modal
