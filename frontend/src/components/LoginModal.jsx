43d6e54fd1f9ae537e52fb2f66c960eea2e1adcc
Filipe Silveira Maciel <filipe10sm@gmail.com>
2024-11-29 22:19:25 -0300

    login estilizado

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -86,16 +86,22 @@ export default function LoginModal({ onLogin }) {
   };
 
   return (
-    <section className="flex items-center justify-center">
-      <div className="flex w-full max-w-4xl items-center gap-8">
+    <section className="flex z- items-center justify-center w-[75vw] sm:w-[60vw] h-[70vh] sm:h-[70vh]">
+
+      <div className="relative w-full h-full  overflow-hidden flex justify-center items-center">
+        {/* Imagem animada */}
         <img
-          src=""
+          src="./telaLogin2.jpg"
           alt="Imagem abstrata da tela de Login"
-          className={`w-1/2 hidden transition-transform duration-300 ease-in-out ${isRegistering ? "order-first" : "order-last"
+          className={`absolute z-10 w-1/2 object-cover contain-size hidden sm:flex sm:w-[30vw] sm:h-[70vh] transition-transform duration-1000 ease-in-out sm:${isRegistering ? "translate-x-[-50%]" : "translate-x-[50%]"
             }`}
         />
 
-        <div className="w-1/2 flex flex-col items-center">
+        {/* Seção do conteúdo animado */}
+        <div
+          className={`absolute  w-1/2 flex flex-col items-center transition-transform duration-1000 ease-in-out sm:${isRegistering ? "translate-x-[50%]" : "translate-x-[-50%]"
+            }`}
+        >
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           {successMessage && <p className="text-green-500">{successMessage}</p>}
 
@@ -109,89 +115,92 @@ export default function LoginModal({ onLogin }) {
                 Sair
               </button>
             </div>
+          ) : isRegistering ? (
+            <div className="flex flex-col w-[65vw] items-center">
+              <h1 className="text-[8vw] font-bold mb-4 text-verde_principal sm:text-[2vw]">&#123; Cadastro &#125;</h1>
+              <h2 className="mb-6 text-white text-[5vw] sm:text-[1vw]">
+                Ou se preferir,{" "}
+                <button
+                  className="text-verde_principal underline text-[5vw] sm:text-[1vw]"
+                  onClick={() => setIsRegistering(false)}
+                >
+                  faça login.
+                </button>
+              </h2>
+              <form className="w-full flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmitRegister)}>
+                <input
+                  type="text"
+                  placeholder="Nome de usuário"
+                  className="border border-gray-300 rounded-md p-2 w-full sm:w-[20vw]"
+                  required
+                  {...register("username")}
+                />
+                <input
+                  type="password"
+                  placeholder="Senha"
+                  className="border border-gray-300 rounded-md p-2 w-full sm:w-[20vw]"
+                  required
+                  {...register("password")}
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="border border-gray-300 rounded-md p-2 w-full sm:w-[20vw]"
+                  required
+                  {...register("email")}
+                />
+                <button
+                  type="submit"
+                  className="w-full sm:w-[20vw] bg-verde_principal/75 text-black rounded-md px-4 py-2 hover:bg-verde_botao cursor-pointer"
+                >
+                  REGISTER
+                </button>
+              </form>
+            </div>
           ) : (
-            isRegistering ? (
-              <div className="flex flex-col items-center">
-                <h1 className="text-[8vw] w-[] font-bold mb-4 text-verde_principal">&#123; Cadastro &#125;</h1>
-                <h2 className="mb-6 text-white text-[5vw]">
-                   Ou se preferir,{" "}
-                  <button
-                    className="text-verde_principal underline text-[5vw]"
-                    onClick={() => setIsRegistering(false)}
-                  >
-                    faça login.
-                  </button>
-                </h2>
-                <form className="w-full flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmitRegister)}>
-                  <input
-                    type="text"
-                    placeholder="Nome de usuário"
-                    className="border border-gray-300 rounded-md p-2 w-full"
-                    required
-                    {...register("username")}
-                  />
-                  <input
-                    type="password"
-                    placeholder="Senha"
-                    className="border border-gray-300 rounded-md p-2 w-full"
-                    required
-                    {...register("password")}
-                  />
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    className="border border-gray-300 rounded-md p-2 w-full"
-                    required
-                    {...register("email")}
-                  />
-                  <button
-                    type="submit"
-                    className="w-[65vw] bg-verde_principal/75 text-black rounded-md px-4 py-2 hover:bg-verde_botao cursor-pointer"
-                  >
-                    REGISTER
-                  </button>
-                </form>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center">
-                <h1 className="text-[8vw] font-bold mb-4 text-verde_principal">&#123; Login &#125;</h1>
-                <h2 className="mb-6 text-white text-[5vw]">
-                  Ou se preferir,{" "}
-                  <button
-                    className="text-verde_principal underline text-[5vw]"
-                    onClick={() => setIsRegistering(true)}
-                  >
-                    cadastre-se.
-                  </button>
-                </h2>
-                <form className="w-full flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmitLogin)}>
-                  <input
-                    type="text"
-                    name="username"
-                    placeholder="Nome de usuário"
-                    className="border border-gray-300 rounded-md p-2 w-[65vw]"
-                    required
-                    {...register("username")}
-                  />
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Senha"
-                    className="border border-gray-300 rounded-md p-2 w-full"
-                    required
-                    {...register("password")}
-                  />
-                  <button type="submit" className="w-full bg-verde_principal/75 text-black rounded-md px-4 py-2 hover:bg-verde_botao cursor-pointer">
-                    ENTER
-                  </button>
-                </form>
-              </div>
-            )
+            <div className="flex flex-col items-center">
+              <h1 className="text-[8vw] font-bold mb-4 text-verde_principal sm:text-[2vw]">&#123; Login &#125;</h1>
+              <h2 className="mb-6 text-white text-[5vw] sm:text-[1vw]">
+                Ou se preferir,{" "}
+                <button
+                  className="text-verde_principal underline text-[5vw] sm:text-[1vw]"
+                  onClick={() => setIsRegistering(true)}
+                >
+                  cadastre-se.
+                </button>
+              </h2>
+              <form className="w-full flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmitLogin)}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Nome de usuário"
+                  className="border border-gray-300 rounded-md p-2 w-[65vw] sm:w-[20vw]"
+                  required
+                  {...register("username")}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Senha"
+                  className="border border-gray-300 rounded-md p-2 w-full sm:w-[20vw] sm:w-[20vw]"
+                  required
+                  {...register("password")}
+                />
+                <button
+                  type="submit"
+                  className="w-full sm:w-[20vw] bg-verde_principal/75 text-black rounded-md px-4 py-2 hover:bg-verde_botao cursor-pointer"
+                >
+                  ENTER
+                </button>
+              </form>
+            </div>
           )}
         </div>
       </div>
 
-          
+
+
+
 
     </section>
   );
