e249a315fcf4dad5bde2d13847c109240dec8f6f
unknown <ericbreinhardt12@gmail.com>
2024-11-29 21:36:49 -0300

    mudança minima

--- frontend/src/components/MenuProjetos.jsx
+++ frontend/src/components/MenuProjetos.jsx
@@ -10,14 +10,13 @@ export default function MenuProjetos () {
 
     return (  
         <header>      
-            <nav 
-            className="flex ">
-                <Link to={profileLink} className={`x-4 ${isActive('/profile')}`}
+            <nav className="flex ">
+                <Link to={profileLink} className={`px-4 ${isActive('/profile')}`}
                 >
                 Perfil
                 </Link>
-                <div className="border-l-6 border-green-500 h-[100vh] absolute left-[10vw] right-[10vw] -ml-[3px] top-0"></div>
-                <Link to={projectsLink} className={`px-4 ${isActive('/projects')}`}
+                
+                <Link to={projectsLink} className={`px-4${isActive('/projects')}`}
                 >
                 Projetos
                 </Link>
