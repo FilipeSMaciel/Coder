886afeb75d470ced8914e010256abf2ba8a8e32f
Pedro Hasse Niemczewski <pedro_hasse@yahoo.com.br>
2024-12-01 05:12:10 -0300

    Freelances almost complete

--- frontend/src/pages/Freelances.jsx
+++ frontend/src/pages/Freelances.jsx
@@ -50,7 +50,7 @@ export default function Freelances() {
             <Menu setOpen={setOpen} />
 
             <main className="font-jetbrains">
-                <section className="h-[25vh] sm:h-[45vh] bg-bg_botao-login flex flex-col items-center justify-center gap-[1vh] sm:gap-[2vh]  border-b-verde_principal border-[0.1vh]">
+                <section className="h-[25vh] sm:h-[45vh] bg-bg_botao-login flex flex-col items-center justify-center gap-[1vh] sm:gap-[2vh] border-b-verde_principal border-[0.1vh]">
                     <h1 className="text-[6vw] sm:text-[3vw] tracking-widest text-verde_principal">Procure um freelance {'>>'}</h1>
                     <h2 className="text-verde_botao text-[4vw] sm:text-[1.6vw] py-2">Pesquise o projeto perfeito para você</h2>
 
@@ -82,7 +82,6 @@ export default function Freelances() {
 
                 <section className="flex flex-col gap-[5vh] pt-[5vh]">
                     <h2 className="text-[5vw] sm:text-[2vw] ml-[4vw] text-verde_principal sm:w-[90vw] border-b-verde_principal border-[0.1rem] border-t-background border-r-background border-l-background ">Mais vistos recentemente {'>>'}</h2>
-                   
                     <h3 className="hidden sm:flex sm:text-[1.5vw] ml-[4vw] text-verde_botao">Com base nos projetos de mais destaque dessa semana</h3>
 
                     {filteredFreelances.map(freelance => (
@@ -105,34 +104,31 @@ export default function Freelances() {
                     ))}
                 </section>
 
-                <section className="flex pt-[5vh] flex-col gap-[5vh] ">
+                <section className="flex flex-col gap-[5vh] pt-[5vh]">
                     <h2 className="text-[5vw] sm:text-[2vw] ml-[4vw] text-verde_principal sm:w-[90vw] border-b-verde_principal border-[0.1rem] border-t-background border-r-background border-l-background">Para você {'>>'}</h2>
-                   
                     <h3 className="hidden sm:flex sm:text-[1.5vw] ml-[4vw] text-verde_botao">Com base nas suas competências e preferências</h3>
 
-                    <div>
-                        <ul>
-                            <li>
-                                <img src="" alt="Logo Projeto" />
-                                <div>
-                                    <h4>Procura-se programadores para Janeiro/25</h4>
-                                    <p>Você deseja participar da CokeJam de Janeiro/25?</p>
-                                    <p>Requisitos: C#, C++, Python, PHP, ORACLE, ANGULAR, JAVA, SQL, Não ser corno</p>
+                    {filteredFreelances.map(freelance => (
+                        <div key={freelance.id} className="bg-bg_botao-login sm:w-[85vw] sm:h-[20vh] sm:m-auto flex justify-around items-center font-inter font-light border-[0.2vh] border[0.5vh] border-verde_botao py-6 drop-shadow-3xl">
+                            <img className={iconProject} src={freelance.image} alt={`${freelance.name} Logo`} />
+                            <div className="flex flex-col items-center justify-center gap-5">
+                                <div className={projectDesc}>
+                                    <h4 className="text-[3vw] sm:text-[1.5vw] text-texto_header">{freelance.name}</h4>
+                                    <p className="text-[2.5vw] sm:text-[1.2vw] m-auto w-[40vw] text-center text-texto_header">{freelance.description}</p>
+                                    <p className="hidden sm:flex text-[2vw] sm:text-[1vw] m-auto mt-5 text-verde_botao">Requisitos: {freelance.requisites}</p>
                                 </div>
-                                <div>
-                                    <div>
-                                        <img src="" alt="Contador de devs icon" />
-                                        <p>15</p>
-                                    </div>
-                                    <a href="#">Saiba Mais ...</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                                <a className="text-verde_principal underline text-[3vw] sm:hidden" href="#">Saiba Mais...</a>
+                            </div>
+                            <div className="flex gap-1 mt-[7vh] sm:w-[8vw] sm:flex-wrap justify-end">
+                                <img className="sm:size-5" src="./Group.svg" alt="Contador de devs icon" />
+                                <p className="text-verde_principal font-extralight">15</p>
+                                <a className="hidden text-verde_principal underline text-[1vw]" href="#">Saiba Mais...</a>
+                            </div>
+                        </div>
+                    ))}
                 </section>
 
-                <div
-                className="flex items-end mb-[2vh] justify-center h-[10vh] text-verde_principal">
+                <div className="flex items-end mb-[2vh] justify-center h-[10vh] text-verde_principal">
                     <p>Próxima página placeholder</p>
                 </div>
             </main>
